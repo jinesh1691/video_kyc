d2f4845fe65596e2569aebbeaac812a899e50855
jinesh1691 <69078281+jinesh1691@users.noreply.github.com>
2020-08-03 11:20:22 +0530

    Update client3.js

--- client3.js
+++ client3.js
@@ -138,7 +138,31 @@ function handleLogin(success) {
             localVideo.srcObject = stream;
             //using Google public stun server 
             var configuration = {
-                "iceServers": [{ "url": "stun:stun2.1.google.com:19302" }]
+                "iceServers": [{ "url": "stun:stun2.1.google.com:19302" },{
+    url: 'turn:numb.viagenie.ca',
+    credential: 'muazkh',
+    username: 'webrtc@live.com'
+},
+{
+    url: 'turn:192.158.29.39:3478?transport=udp',
+    credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+    username: '28224511:1379330808'
+},
+{
+    url: 'turn:192.158.29.39:3478?transport=tcp',
+    credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+    username: '28224511:1379330808'
+},
+{
+    url: 'turn:turn.bistri.com:80',
+    credential: 'homeo',
+    username: 'homeo'
+ },
+ {
+    url: 'turn:turn.anyfirewall.com:443?transport=tcp',
+    credential: 'webrtc',
+    username: 'webrtc'
+}]
             };
 
             yourConn = new webkitRTCPeerConnection(configuration);
